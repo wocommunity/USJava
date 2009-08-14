8094c117cda02f1051952d29524eb6dba690f48a
Bjarni Sævarsson <bjarnisaevarsson@gmail.com>
2009-08-14 11:28:49 +0000

    INN707 - updated * encodeImageToJPEGData now just tests if the image is a functional jpeg. * testScale: tests for height and width. * thumbnaleScale: tests height and width

--- Tests/is/us/util/TestUSImageUtilities.java
+++ Tests/is/us/util/TestUSImageUtilities.java
@@ -1,30 +1,29 @@
 package is.us.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import is.us.thirdparty.ImageInfo;
 import is.us.util.USImageUtilities.CodecType;
 
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.File;
 
 import javax.imageio.ImageIO;
 
 import org.junit.Test;
 
 public class TestUSImageUtilities {
-	private static final String CHECKSUM_TEST_IMAGE_ORIGINAL = "2bd7091ec2a5ffeaaf637c4cf141cbd2";
-	private static final String TEST_IMAGE_ORIGINAL = "usimageutilities_original.jpg";
-
-	private static final String TEST_IMAGE_QUALITY_10 = "usimageutilities_quality_10.jpg";
-	private static final String TEST_IMAGE_QUALITY_50 = "usimageutilities_quality_50.jpg";
-	private static final String TEST_IMAGE_SCALE_150X150 = "usimageutilities_scale_150x150.jpg";
-	private static final String TEST_IMAGE_THUMBNAIL_100X100 = "usimageutilities_thumbnail_100x100.jpg";
-	private static final String originalPath = USTestUtilities.documentPath( TestUSImageUtilities.class, TEST_IMAGE_ORIGINAL );
+	private static final String TEST_IMAGE_NAME = "usimageutilities_original.jpg";
+	private static final String TEST_IMAGE_PATH = USTestUtilities.documentPath( TestUSImageUtilities.class, TEST_IMAGE_NAME );
 
 	@Test
 	public void testOriginal() {
-		String t1Path = USTestUtilities.documentPath( this.getClass(), TEST_IMAGE_ORIGINAL );
 		try {
-			assertEquals( CHECKSUM_TEST_IMAGE_ORIGINAL, USTestUtilities.streamDigest( t1Path ) );
+			BufferedImage bi2 = ImageIO.read( new File( TEST_IMAGE_PATH ) );
+			byte[] bytes50 = USImageUtilities.encode( bi2, 50, CodecType.JPEG );
+			ImageInfo info = USImageUtilities.imageInfo( bytes50 );
+			assertEquals( info.getFormat(), ImageInfo.FORMAT_JPEG );
+			assertEquals( info.getWidth(), 126 );
 		}
 		catch( Exception e ) {
 			fail( e.getMessage() );
@@ -33,21 +32,18 @@ public class TestUSImageUtilities {
 
 	@Test
 	public void encode() {
-		String quality10Path = USTestUtilities.documentPath( this.getClass(), TEST_IMAGE_QUALITY_10 );
-		String quality50Path = USTestUtilities.documentPath( this.getClass(), TEST_IMAGE_QUALITY_50 );
-
 		try {
 			// test 10% quality
-			BufferedImage bi = ImageIO.read( new File( originalPath ) );
-			byte[] bytes = USImageUtilities.encode( bi, 10, CodecType.JPEG );
-			InputStream inStream1 = new ByteArrayInputStream( bytes );
-			assertEquals( USTestUtilities.streamDigest( quality10Path ), USTestUtilities.streamDigest( inStream1 ) );
+			BufferedImage bi = ImageIO.read( new File( TEST_IMAGE_PATH ) );
+			byte[] bytes10 = USImageUtilities.encode( bi, 10, CodecType.JPEG );
+			ImageInfo info = USImageUtilities.imageInfo( bytes10 );
+			assertEquals( info.getFormat(), ImageInfo.FORMAT_JPEG );
 
 			// test 50% quality
-			BufferedImage bi2 = ImageIO.read( new File( originalPath ) );
+			BufferedImage bi2 = ImageIO.read( new File( TEST_IMAGE_PATH ) );
 			byte[] bytes50 = USImageUtilities.encode( bi2, 50, CodecType.JPEG );
-			InputStream inStream50 = new ByteArrayInputStream( bytes50 );
-			assertEquals( USTestUtilities.streamDigest( quality50Path ), USTestUtilities.streamDigest( inStream50 ) );
+			info = USImageUtilities.imageInfo( bytes50 );
+			assertEquals( info.getFormat(), ImageInfo.FORMAT_JPEG );
 		}
 		catch( Exception e ) {
 			fail( e.getMessage() );
@@ -56,12 +52,12 @@ public class TestUSImageUtilities {
 
 	@Test
 	public void testScale() {
-		String testPath = USTestUtilities.documentPath( this.getClass(), TEST_IMAGE_SCALE_150X150 );
 		try {
-			byte[] input = USDataUtilities.readBytesFromFile( new File( originalPath ) );
+			byte[] input = USDataUtilities.readBytesFromFile( new File( TEST_IMAGE_PATH ) );
 			byte[] bytes = USImageUtilities.scale( input, 150, 150, 100, CodecType.JPEG );
-			InputStream inStream = new ByteArrayInputStream( bytes );
-			assertEquals( USTestUtilities.streamDigest( testPath ), USTestUtilities.streamDigest( inStream ) );
+			ImageInfo info = USImageUtilities.imageInfo( bytes );
+			assertEquals( info.getWidth(), 150 );
+			assertEquals( info.getHeight(), 150 );
 		}
 		catch( Exception e ) {
 			fail( e.getMessage() );
@@ -70,26 +66,16 @@ public class TestUSImageUtilities {
 
 	@Test
 	public void thumbnaleScale() {
-		String testPath = USTestUtilities.documentPath( this.getClass(), TEST_IMAGE_THUMBNAIL_100X100 );
 		try {
-			byte[] input = USDataUtilities.readBytesFromFile( new File( testPath ) );
+			byte[] input = USDataUtilities.readBytesFromFile( new File( TEST_IMAGE_PATH ) );
 			byte[] bytes = USImageUtilities.createThumbnail( input, 50, 50, 100, CodecType.PNG );
-			InputStream inStream = new ByteArrayInputStream( bytes );
-			assertEquals( USTestUtilities.streamDigest( testPath ), USTestUtilities.streamDigest( inStream ) );
+			ImageInfo info = USImageUtilities.imageInfo( bytes );
+			assertEquals( info.getWidth(), 50 );
+			assertEquals( info.getHeight(), 37 );
 		}
 		catch( Exception e ) {
 			fail( e.getMessage() );
 		}
 	}
 
-	public void saveFile( InputStream inStream, String fileName ) throws IOException {
-		File file = new File( fileName );
-		OutputStream out = new FileOutputStream( file );
-		byte buf[] = new byte[8192];
-		int len;
-		while( (len = inStream.read( buf )) > 0 ) {
-			out.write( buf, 0, len );
-		}
-		out.close();
-	}
 }
