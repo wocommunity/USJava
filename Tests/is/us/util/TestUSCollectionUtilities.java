cdd14ef7fb073fc4b30154c570f05ced7d3b438e
Hugi Thordarson <hugi@karlmenn.is>
2010-05-17 09:51:57 +0000

    Added tests for USCollectionUtilities

--- Tests/is/us/util/TestUSCollectionUtilities.java
+++ Tests/is/us/util/TestUSCollectionUtilities.java
@@ -38,4 +38,34 @@ public class TestUSCollectionUtilities {
 
 		return a;
 	}
+
+	@Test
+	public void searchUnsorted() {
+		String[] testArr = new String[2];
+		testArr[0] = "index 0";
+		testArr[1] = "index 1";
+		assertEquals( 1, USCollectionUtilities.searchUnsorted( testArr, "index 1" ) );
+		assertEquals( -1, USCollectionUtilities.searchUnsorted( testArr, "index 2" ) );
+		assertEquals( -1, USCollectionUtilities.searchUnsorted( null, "index 2" ) );
+		assertEquals( -1, USCollectionUtilities.searchUnsorted( testArr, null ) );
+		assertEquals( -1, USCollectionUtilities.searchUnsorted( null, null ) );
+	}
+
+	@Test
+	public void resize() {
+		String[] oldArr = new String[2];
+		oldArr[0] = "index 0";
+		oldArr[1] = "index 1";
+		String[] newArr = USCollectionUtilities.resize( oldArr, 4 );
+
+		assertEquals( 4, newArr.length );
+		assertEquals( oldArr[0], "index 0" );
+		assertEquals( oldArr[1], "index 1" );
+
+		newArr = USCollectionUtilities.resize( oldArr, 0 );
+		assertEquals( newArr.length, 0 );
+
+		newArr = USCollectionUtilities.resize( oldArr, -1 );
+		assertEquals( newArr.length, 0 );
+	}
 }
